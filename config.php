1832aadfbb2d336b7ebe6e9a15f4c16de1673e18
jcjj <jcjj.system@gmail.com>
2012-07-23 15:18:25 -0400

    New instructions
    instructions for config.php

--- config.php
+++ config.php
@@ -1,6 +1,6 @@
 <?php
 
-// You must create a new client at http://instagram.com/developer/ and put the client info below
+// You must create a new client at http://instagram.com/developer/ and put the client info below (replace YOUR-CLIENT-ID,YOUR_CLIENT-SECRET,USER-TARGET-ID)
 define('CLIENT_ID','YOUR-CLIENT-ID');
 define('CLIENT_SECRET','YOUR_CLIENT-SECRET');
 define('REDIRECT_URI','https://full.path/to/callback.php');
